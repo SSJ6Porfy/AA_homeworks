a3d200c8e53910091754126229d913e612a94141
Porfirio Matias <ssj6porfy@hotmail.com>
2017-08-22 20:46:56 -0700

    add W4D3 hmk

--- W4D1/config/routes.rb
+++ W4D1/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :pages do
+    resources :pages, only: [:index, :destroy]
+  end
+
   resources :books, only: [:new, :create, :index, :destroy]
 end
