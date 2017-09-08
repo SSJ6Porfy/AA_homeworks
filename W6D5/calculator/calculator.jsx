5dd8d0626754b9834e22f376ce7c3313923072c7
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-08 09:12:40 -0700

    finish calc

--- W6D5/calculator/calculator.jsx
+++ W6D5/calculator/calculator.jsx
@@ -3,16 +3,29 @@ import React from 'react';
 class Calculator extends React.Component{
   constructor(props){
     super(props);
-    //your code here
+    this.state = { userInput1: "", userInput2: "", result: 0 }
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
   }
 
-  //your code here
+  setNum1(e) {
+    this.state.userInput1 = e
+  }
 
+  setNum2(e) {
+    this.state.userInput2 = e
+  }
   render(){
     return (
       <div>
-        <h1>Hello World</h1>
-        //your code will replace this
+        <h1>{this.state.result}</h1>
+        <br/>
+        <input type="text" value={this.state.userInput1}></input>
+        <br/>
+        <input type="text" value={this.state.userInput2}></input>
+        <br/>
+        <br/>
+        <span>Options: [ + - / * ] </span>
       </div>
     );
   }
