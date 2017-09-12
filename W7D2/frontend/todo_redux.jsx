9961e9cc574dfabd99a7ed9b9bf2713a5c30dca5
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-11 22:50:30 -0700

    Finished W7D2 hmk

--- W7D2/frontend/todo_redux.jsx
+++ W7D2/frontend/todo_redux.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
-
 import Root from './components/root';
 
 // const addLoggingToDispatch = (store) => {
@@ -16,28 +15,28 @@ import Root from './components/root';
 //   }
 // };
 //
-const addLoggingToDispatch = store => next => action => {
-  console.log(store.getState());
-  console.log(action);
-  next(action);
-  console.log(store.getState());
-};
-
-const applyMiddlewares = (store, ...middlewares) => {
-  let dispatch = store.dispatch;
-
-  middlewares.forEach((mid) => {
-    dispatch = mid(store)(dispatch);
-  });
-  return Object.assign({}, store, { dispatch })
-};
+// const addLoggingToDispatch = store => next => action => {
+//   console.log(store.getState());
+//   console.log(action);
+//   next(action);
+//   console.log(store.getState());
+// };
+//
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch;
+//
+//   middlewares.forEach((mid) => {
+//     dispatch = mid(store)(dispatch);
+//   });
+//   return Object.assign({}, store, { dispatch })
+// };
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   let store = configureStore(preloadedState);
 
-  store = applyMiddlewares(store, addLoggingToDispatch);
+  // store = applyMiddlewares(store, addLoggingToDispatch);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
