0a26b8e2c6cc7896d696475e6711ed0838d495e7
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-11 22:45:19 -0700

    Finished W7D2 hmk

--- W7D2/frontend/todo_redux.jsx
+++ W7D2/frontend/todo_redux.jsx
@@ -2,12 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
+
 import Root from './components/root';
+
+// const addLoggingToDispatch = (store) => {
+//   const newStore = store.dispatch
+//
+//   return function(action) {
+//     console.log(store.getState());
+//     console.log(action);
+//     newStore(action);
+//     console.log(store.getState());
+//   }
+// };
+//
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+
+  middlewares.forEach((mid) => {
+    dispatch = mid(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch })
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+  let store = configureStore(preloadedState);
+
+  store = applyMiddlewares(store, addLoggingToDispatch);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
