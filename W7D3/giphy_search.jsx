74eb3c4382240b7a7f04a1adcdf5ec15295f9a37
Porfirio Matias <ssj6porfy@hotmail.com>
2017-09-13 00:12:27 -0700

    finished Phase 1

--- W7D3/giphy_search.jsx
+++ W7D3/giphy_search.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { fetchSearchGiphys } from "./util/api_util";
-
+import { fetchSearchGiphys } from "./actions/giphy_actions";
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
-  ReactDOM.render(<Root store={store}/>, document.getElementById('content'));
-  
+
+  // ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
+  window.store = store
+  window.fetchSearchGiphys = fetchSearchGiphys;
+
 });
